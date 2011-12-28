9d996b2fce2f264acf7efea16e00090ea3dd8e26
Tim White <tim@cyface.com>
2011-12-28 14:43:46 -0700

    Fixed boilerplate.

--- socialprofile/wsgi.py
+++ socialprofile/wsgi.py
@@ -4,5 +4,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
 
 from django.core.handlers.wsgi import WSGIHandler
-os.environ["DJANGO_SETTINGS_MODULE"] = "myproject.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "socialprofile.settings"
 application = WSGIHandler()
