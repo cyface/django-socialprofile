04e659b922640882b2ed4871bd09f52142f9f702
Tim White <tim@cyface.com>
2011-12-28 14:26:26 -0700

    Removed virtualenv from SCC.

--- socialprofile-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/cyface/Dropbox/Personal/Cyface/Aptana3Workspace/django-socialprofile/socialprofile-env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- socialprofile-env/lib/python2.7/site-packages/social_auth/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Django-social-auth application, allows OpenId or OAuth user
-registration/authentication just adding a few configurations.
-"""
-version = (0, 6, 1)
-__version__ = '.'.join(map(str, version))
