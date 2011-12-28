5f1734ac0c3dc2fd952b41de9d79548d94489bcc
Tim White <tim@cyface.com>
2011-12-28 14:41:42 -0700

    Initial cut at wsgi.py for Gondor.

---
+++ socialprofile/wsgi.py
@@ -0,0 +1,8 @@
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
+
+from django.core.handlers.wsgi import WSGIHandler
+os.environ["DJANGO_SETTINGS_MODULE"] = "myproject.settings"
+application = WSGIHandler()
