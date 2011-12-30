4c599eb0ec8b1a54099b9910801523f1954bc0a3
Tim White <tim@cyface.com>
2011-12-30 07:27:00 -0700

    Removed buildout, added setupscripts for virtualenv.

---
+++ devscripts/setup_dev_env.sh
@@ -0,0 +1,7 @@
+# You must have virtualenv installed, and the virualenv command in your path for this to work.
+# Assuming you have python installed, you can install virtualenv using the command below.
+# curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
+
+virtualenv socialprofile-env
+. socialprofile-env/bin/activate
+pip install -r socialprofile/requirements_dev.txt
