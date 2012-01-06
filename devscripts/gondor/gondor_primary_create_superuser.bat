fa7c8aaca2ac9ea3d6ff313ab8450784f85f3596
cyface <tim@cyface.com>
2012-01-06 16:09:54 -0700

    Updated bat files for gondor

--- devscripts/gondor/gondor_primary_create_superuser.bat
+++ devscripts/gondor/gondor_primary_create_superuser.bat
@@ -1,4 +1,4 @@
 REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
 call ..\socialprofile-env\Scripts\activate.bat
-REM This deploys the currently checked-in GIT Master Branch to the primary Gondor Instance
-gondor deploy primary master
+REM Creates Superuser in primary Gondor Instance
+gondor run primary createsuperuser
