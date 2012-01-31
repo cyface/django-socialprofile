0e77961104809f8dda10fde425b415b5d98e695a
Tim White <tim@cyface.com>
2012-01-30 20:21:49 -0700

    2nd Cut at T&C module with Brant's input.

--- socialprofile/termsandconditions/models.py
+++ socialprofile/termsandconditions/models.py
@@ -4,20 +4,47 @@
 
 from django.contrib.auth.models import User
 from django.db import models
+import datetime
 import logging
 
 log = logging.getLogger(name='termsandconditions')
 
 class UserTermsAndConditions(models.Model):
     """Holds mapping between TermsAndConditions and Users"""
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, related_name="userterms")
     terms = models.OneToOneField("TermsAndConditions", related_name="terms")
-    ip_address = models.DecimalField(null=True, decimal_places=2, max_digits=6)
+    ip_address = models.IPAddressField(null=True, blank=True)
     date_accepted = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        get_latest_by = 'date_accepted'
+
 class TermsAndConditions(models.Model):
-    """Holds Versions of TermsAndConditions"""
-    usertermsandconditions = models.ManyToManyField(User, through=UserTermsAndConditions)
+    """Holds Versions of TermsAndConditions
+    Active one for a given slug is: date_active is not Null and is latest not in future"""
+    slug = models.SlugField()
+    name = models.TextField(max_length=255)
+    users = models.ManyToManyField(User, through=UserTermsAndConditions, blank=True, null=True,)
     version_number = models.DecimalField(default=1.0, decimal_places=2, max_digits=6)
-    text = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(null=True, blank=True)
+    date_active = models.DateTimeField(blank=True, null=True, help_text="Leave Null To Never Make Active")
+    date_created = models.DateTimeField(blank=True, auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_active',]
+        get_latest_by = 'date_active'
+
+    @staticmethod
+    def get_active(slug):
+        """Finds the latest of a particular terms and conditions"""
+        return TermsAndConditions.objects.filter(date_active__isnull=False, date_active__lte=datetime.datetime.now(), slug=slug).latest('date_active')
+
+    @staticmethod
+    def agreed_to_latest(user, slug):
+        """Checks to see if a specified user has agreed to the latest of a particular terms and conditions"""
+        try:
+            UserTermsAndConditions.objects.get(user=user, terms=TermsAndConditions.get_active(slug))
+
+            return True
+        except UserTermsAndConditions.DoesNotExist:
+            return False
--- socialprofile/termsandconditions/tests.py
+++ socialprofile/termsandconditions/tests.py
@@ -13,14 +13,29 @@ class TermsAndConditionsTests(unittest.TestCase):
         """Setup for each test"""
         self.user1 = User.objects.create(username='user1')
         self.user2 = User.objects.create(username='user2')
-        self.terms1 = TermsAndConditions.objects.create(text="Terms and Conditions1", version_number=1.0)
-        self.terms2 = TermsAndConditions.objects.create(text="Terms and Conditions2", version_number=2.0)
+        self.terms1 = TermsAndConditions.objects.create(slug="siteterms", name="Site Terms", text="Terms and Conditions1", version_number=1.0, date_active="2012-01-01")
+        self.terms2 = TermsAndConditions.objects.create(slug="siteterms", name="Site Terms", text="Terms and Conditions2", version_number=2.0, date_active="2012-01-05")
+        self.terms3 = TermsAndConditions.objects.create(slug="contribterms", name="Contributor Terms", text="Terms and Conditions1", version_number=1.5, date_active="2012-01-01")
+        self.terms4 = TermsAndConditions.objects.create(slug="contribterms", name="Contributor Terms", text="Terms and Conditions1", version_number=2.0, date_active="2100-01-01")
 
-    def test_accept_terms(self):
-        """Accept Terms"""
+
+    def test_terms_and_conditions(self):
+        """Various tests of the TermsAndConditions Module"""
+
+        """Testing Direct Assignment of Acceptance"""
         UserTermsAndConditions.objects.create(user=self.user1, terms=self.terms1)
-        UserTermsAndConditions.objects.create(user=self.user2, terms=self.terms2)
+        UserTermsAndConditions.objects.create(user=self.user2, terms=self.terms3)
 
-        self.assertEquals(1.0, self.user1.usertermsandconditions.terms.version_number)
-        self.assertEquals(2.0, self.user2.usertermsandconditions.terms.version_number)
+        self.assertEquals(1.0, self.user1.userterms.terms.version_number)
+        self.assertEquals(1.5, self.user2.userterms.terms.version_number)
+
+        self.assertEquals('user1', self.terms1.users.all()[0].username)
+
+        """Testing the get_active static method of TermsAndConditions"""
+        self.assertEquals(2.0, TermsAndConditions.get_active(slug='siteterms').version_number)
+        self.assertEquals(1.5, TermsAndConditions.get_active(slug='contribterms').version_number)
+
+        """Testing the agreed_to_latest static method of TermsAndConditions"""
+        self.assertEquals(False, TermsAndConditions.agreed_to_latest(user=self.user1, slug='siteterms'))
+        self.assertEquals(True, TermsAndConditions.agreed_to_latest(user=self.user2, slug='contribterms'))
 
