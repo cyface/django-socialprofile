28480d435ec896102c73c9e6a3d7047c70d9bd6b
cyface <tim@cyface.com>
2012-01-31 10:16:13 -0700

    Commented out test that has to be run with a fresh google key.

--- socialprofile/tests.py
+++ socialprofile/tests.py
@@ -7,20 +7,20 @@ from urllib import urlencode
 from urllib2 import Request, urlopen
 from django.utils import simplejson
 
-class OauthGoogleTestCase(unittest.TestCase):
-    """Tests Google Oauth for extra values, you need to look in user_auth tables for an access_token"""
-    def testGetUserData(self):
-        """Test user data from Google oauth"""
-        access_token = 'GETAFRESHONE'
-
-        user_info_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-
-        data = {'access_token': access_token, 'alt': 'json'}
-        params = urlencode (data)
-        request = Request(user_info_url + '?' + params, headers={'Authorization': params})
-        result =  simplejson.loads(urlopen(request).read())
-
-        print result
-
-        print result['family_name']
+#class OauthGoogleTestCase(unittest.TestCase):
+#    """Tests Google Oauth for extra values, you need to look in user_auth tables for an access_token"""
+#    def testGetUserData(self):
+#        """Test user data from Google oauth"""
+#        access_token = 'GETAFRESHONE'
+#
+#        user_info_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+#
+#        data = {'access_token': access_token, 'alt': 'json'}
+#        params = urlencode (data)
+#        request = Request(user_info_url + '?' + params, headers={'Authorization': params})
+#        result =  simplejson.loads(urlopen(request).read())
+#
+#        print result
+#
+#        print result['family_name']
 
