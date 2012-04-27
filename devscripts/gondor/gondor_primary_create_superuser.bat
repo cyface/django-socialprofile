9f24583e9542dbbfeb7384460e4352aa901a7940
cyface <tim@cyface.com>
2012-04-27 13:42:08 -0600

    Moved env directory to django-socialprofile-env, updated scripts to match.

--- devscripts/gondor/gondor_primary_create_superuser.bat
+++ devscripts/gondor/gondor_primary_create_superuser.bat
@@ -1,4 +1,4 @@
 REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
-call ..\socialprofile-env\Scripts\activate.bat
+call ..\django-socialprofile-env\Scripts\activate.bat
 REM Creates Superuser in primary Gondor Instance
 gondor run primary createsuperuser
