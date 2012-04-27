7e1e52828b8165bbaa68d821670c972d3eaa03b9
cyface <tim@cyface.com>
2012-04-27 14:12:51 -0600

    Tweaking path definition for gondor.

--- socialprofile/socialprofile-demo/manage.py
+++ socialprofile/socialprofile-demo/manage.py
@@ -3,6 +3,7 @@
 from django.core.management import execute_manager
 #try: # Try/except removed to make sure we see the errors when deploying to Gondor
 import settings # Assumed to be in the same directory.
+print sys.path
 #except ImportError:
 #    import sys
 #    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
