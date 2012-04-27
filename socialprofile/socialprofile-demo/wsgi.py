06bf4e1a685a99f7522883cdede594446306795d
cyface <tim@cyface.com>
2012-04-27 13:54:46 -0600

    Tweaking settings definition for gondor.

--- socialprofile/socialprofile-demo/wsgi.py
+++ socialprofile/socialprofile-demo/wsgi.py
@@ -8,5 +8,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
 
 from django.core.handlers.wsgi import WSGIHandler
-os.environ["DJANGO_SETTINGS_MODULE"] = "socialprofile.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "socialprofile-demo.settings"
 application = WSGIHandler()
