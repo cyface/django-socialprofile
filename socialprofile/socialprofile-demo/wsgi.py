1f38e0741c82f654bd608b8f8230d3fd8816edf8
cyface <tim@cyface.com>
2012-04-27 14:06:41 -0600

    Tweaking path definition for gondor.

--- socialprofile/socialprofile-demo/wsgi.py
+++ socialprofile/socialprofile-demo/wsgi.py
@@ -4,9 +4,11 @@
 
 import os, sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir), os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.pardir), os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
+
+print (sys.path)
 
 from django.core.handlers.wsgi import WSGIHandler
 os.environ["DJANGO_SETTINGS_MODULE"] = "socialprofile-demo.settings"
