db45aa84edce8ca5d51a42f4f02465ae28a1273f
cyface <tim@cyface.com>
2012-04-27 14:02:01 -0600

    Tweaking path definition for gondor.

--- socialprofile/socialprofile-demo/wsgi.py
+++ socialprofile/socialprofile-demo/wsgi.py
@@ -4,7 +4,7 @@
 
 import os, sys
 
-sys.path.insert(0, os.path.abspath(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)), os.pardir))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir), os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
 
