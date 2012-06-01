cb2efbeff50cab79e023725ec84912e8ad8786af
cyface <tim@cyface.com>
2012-06-01 11:23:32 -0600

    Updated for latest Gondor setup.

---
+++ socialprofile/socialprofile_demo/views.py
@@ -0,0 +1,21 @@
+"""Django Views for the socialprofile-demo module"""
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import logging
+
+logger = logging.getLogger(name='socialprofile')
+
+def index(request):
+    """
+    Main site page page.
+
+    url: /
+    
+    template : templates/index.html
+    """
+
+    logger.debug('indexpage')
+
+    response_data = {}
+
+    return render_to_response('index.html', response_data, context_instance=RequestContext(request))
