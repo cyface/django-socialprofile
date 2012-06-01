a03cd1dc10b210f6e99a1978b2262619a4b0ca0b
cyface <tim@cyface.com>
2012-06-01 12:00:39 -0600

    Updated for latest Gondor setup.

--- devscripts/gondor/gondor_deploy_primary_master.bat
+++ devscripts/gondor/gondor_deploy_primary_master.bat
@@ -1,4 +1,4 @@
 REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
-call ..\django-socialprofile-env\Scripts\activate.bat
+call ..\..\django-socialprofile-env\Scripts\activate.bat
 REM This deploys the currently checked-in GIT Master Branch to the primary Gondor Instance
 gondor deploy primary master
