541a31b444ee2a24925aa30216b2210c09b8cf45
cyface <tim@cyface.com>
2012-06-01 17:52:17 -0600

    Updated for latest Gondor setup.

--- socialprofile_demo/settings_local_template.py
+++ socialprofile_demo/settings_local_template.py
@@ -74,6 +74,9 @@ DATABASES = {
         }
 }
 
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'socialprofile_demo.wsgi.application'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -166,16 +169,31 @@ if DEBUG:
     }
 
 #### LOGGING
-#if DEBUG:
-#    import logging
-#    import logging.handlers
-#
-#    rotating_handler = logging.handlers.RotatingFileHandler(os.path.join(PROJECT_ROOT, 'django.log'), maxBytes=200000, backupCount=5)
-#    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#    rotating_handler.setFormatter(formatter)
-#    logging.getLogger('').addHandler(rotating_handler)
-#
-#    logging.getLogger(name='django').setLevel(logging.ERROR)
-#    sp_logger = logging.getLogger(name='socialprofile')
-#    sp_logger.setLevel(logging.DEBUG)
-#    sp_logger.debug('Logging Startup')
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+            },
+        }
+}
--- socialprofile_demo/wsgi.py
+++ socialprofile_demo/wsgi.py
@@ -5,11 +5,14 @@
 import os, sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.pardir), os.pardir)))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
 
 print (sys.path)
 
-from django.core.handlers.wsgi import WSGIHandler
-os.environ["DJANGO_SETTINGS_MODULE"] = 'socialprofile_demo.settings'
-application = WSGIHandler()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'socialprofile_demo.settings')
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
