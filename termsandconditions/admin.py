505430ae116448df38b7825d7980228f206d7b03
Tim White <tim@cyface.com>
2012-06-06 22:52:33 -0600

    Updated Terms&Conditions to display and save.  Next: add auto-check hook.

--- termsandconditions/admin.py
+++ termsandconditions/admin.py
@@ -3,10 +3,19 @@
 # pylint: disable=R0904
 
 from django.contrib import admin
-from models import TermsAndConditions
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from models import TermsAndConditions, UserTermsAndConditions
     
 class TermsAndConditionsAdmin(admin.ModelAdmin):
-    """Sets up the custom user admin display"""
+    """Sets up the custom Terms and Conditions admin display"""
     list_display = ('slug', 'name', 'date_active', 'version_number',)
 
+class UserTermsAndConditionsAdmin(admin.ModelAdmin):
+    """Sets up the custom User Terms and Conditions admin display"""
+    list_display = ('terms', 'user', 'date_accepted', 'ip_address',)
+    date_hierarchy = 'date_accepted'
+    list_select_related = True
+
 admin.site.register(TermsAndConditions, TermsAndConditionsAdmin)
+admin.site.register(UserTermsAndConditions, UserTermsAndConditionsAdmin)
