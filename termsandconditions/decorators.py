60485077cb1ecbf1140c912ba7a77132a007f195
Tim White <tim@cyface.com>
2012-06-08 21:54:00 -0600

    Updated tests.

--- termsandconditions/decorators.py
+++ termsandconditions/decorators.py
@@ -1,30 +1,33 @@
 import urlparse
+from functools import wraps
 from django.http import HttpResponseRedirect, QueryDict
 from django.conf import settings
 from django.utils.decorators import available_attrs
+from models import TermsAndConditions
 
 if hasattr(settings, 'ACCEPT_TERMS_PATH'):
     ACCEPT_TERMS_PATH = settings.ACCEPT_TERMS_PATH
 else:
     ACCEPT_TERMS_PATH = '/terms/accept/'
 
-def terms_required (required_func):
+if hasattr(settings, 'TERMS_RETURNTO_PARAM'):
+    TERMS_RETURNTO_PARAM = settings.TERMS_RETURNTO_PARAM
+else:
+    TERMS_RETURNTO_PARAM = 'returnTo'
+
+def terms_required(view_func):
     """
     This decorator checks to see if the user is logged in, and if so, if they have accepted the site terms.
     """
+    @wraps(view_func, assigned=available_attrs(view_func))
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated() or TermsAndConditions.agreed_to_latest(request.user):
+            return view_func(request, *args, **kwargs)
 
-    def decorator(view_func):
-        @wraps(view_func, assigned=available_attrs(view_func))
-        def _wrapped_view(request, *args, **kwargs):
-            if not request.user.is_authenticated():
-                return view_func(request, *args, **kwargs)
-
-            currentPath = request.META['PATH_INFO']
-            login_url_parts = list(urlparse.urlparse(ACCEPT_TERMS_PATH))
-            querystring = QueryDict(login_url_parts[4], mutable=True)
-            querystring['returnTo'] = currentPath
-            login_url_parts[4] = querystring.urlencode(safe='/')
-            return HttpResponseRedirect(urlparse.urlunparse(login_url_parts))
-
-        return _wrapped_view
-    return decorator
+        currentPath = request.META['PATH_INFO']
+        login_url_parts = list(urlparse.urlparse(ACCEPT_TERMS_PATH))
+        querystring = QueryDict(login_url_parts[4], mutable=True)
+        querystring['returnTo'] = currentPath
+        login_url_parts[4] = querystring.urlencode(safe='/')
+        return HttpResponseRedirect(urlparse.urlunparse(login_url_parts))
+    return _wrapped_view
--- termsandconditions/tests.py
+++ termsandconditions/tests.py
@@ -13,29 +13,40 @@ class TermsAndConditionsTests(unittest.TestCase):
         """Setup for each test"""
         self.user1 = User.objects.create(username='user1')
         self.user2 = User.objects.create(username='user2')
-        self.terms1 = TermsAndConditions.objects.create(slug="siteterms", name="Site Terms", text="Terms and Conditions1", version_number=1.0, date_active="2012-01-01")
-        self.terms2 = TermsAndConditions.objects.create(slug="siteterms", name="Site Terms", text="Terms and Conditions2", version_number=2.0, date_active="2012-01-05")
-        self.terms3 = TermsAndConditions.objects.create(slug="contribterms", name="Contributor Terms", text="Terms and Conditions1", version_number=1.5, date_active="2012-01-01")
-        self.terms4 = TermsAndConditions.objects.create(slug="contribterms", name="Contributor Terms", text="Terms and Conditions1", version_number=2.0, date_active="2100-01-01")
+        self.terms1 = TermsAndConditions.objects.create(slug="site-terms", name="Site Terms", text="Terms and Conditions1", version_number=1.0, date_active="2012-01-01")
+        self.terms2 = TermsAndConditions.objects.create(slug="site-terms", name="Site Terms", text="Terms and Conditions2", version_number=2.0, date_active="2012-01-05")
+        self.terms3 = TermsAndConditions.objects.create(slug="contrib-terms", name="Contributor Terms", text="Terms and Conditions1", version_number=1.5, date_active="2012-01-01")
+        self.terms4 = TermsAndConditions.objects.create(slug="contrib-terms", name="Contributor Terms", text="Terms and Conditions1", version_number=2.0, date_active="2100-01-01")
 
+    def tearDown(self):
+        """Teardown for each test"""
+        User.objects.all().delete()
+        TermsAndConditions.objects.all().delete()
+        UserTermsAndConditions.objects.all().delete()
 
     def test_terms_and_conditions(self):
         """Various tests of the TermsAndConditions Module"""
 
         # Testing Direct Assignment of Acceptance
-        UserTermsAndConditions.objects.create(user=self.user1, terms=self.terms1)
+        self.userterms1 = UserTermsAndConditions.objects.create(user=self.user1, terms=self.terms1)
         UserTermsAndConditions.objects.create(user=self.user2, terms=self.terms3)
 
-        self.assertEquals(1.0, self.user1.userterms.terms.version_number)
-        self.assertEquals(1.5, self.user2.userterms.terms.version_number)
+
+        self.assertEquals(1.0, self.user1.userterms.get().terms.version_number)
+        self.assertEquals(1.5, self.user2.userterms.get().terms.version_number)
 
         self.assertEquals('user1', self.terms1.users.all()[0].username)
 
         # Testing the get_active static method of TermsAndConditions
-        self.assertEquals(2.0, TermsAndConditions.get_active(slug='siteterms').version_number)
-        self.assertEquals(1.5, TermsAndConditions.get_active(slug='contribterms').version_number)
+        self.assertEquals(2.0, TermsAndConditions.get_active(slug='site-terms').version_number)
+        self.assertEquals(1.5, TermsAndConditions.get_active(slug='contrib-terms').version_number)
 
         # Testing the agreed_to_latest static method of TermsAndConditions
-        self.assertEquals(False, TermsAndConditions.agreed_to_latest(user=self.user1, slug='siteterms'))
-        self.assertEquals(True, TermsAndConditions.agreed_to_latest(user=self.user2, slug='contribterms'))
+        self.assertEquals(False, TermsAndConditions.agreed_to_latest(user=self.user1, slug='site-terms'))
+        self.assertEquals(True, TermsAndConditions.agreed_to_latest(user=self.user2, slug='contrib-terms'))
+
+    def test_terms_and_conditions_urls(self):
+        def testTermsRequiredRedirect(self):
+            response = self.c.get('/terms/required/', follow=True)
+            self.assertRedirects(response, "http://testserver/terms/accept/?returnTo=/terms/required/")
 
