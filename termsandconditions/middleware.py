469e49f5f52f6eb933989b0bec54170e3900a5ab
Tim White <tim@cyface.com>
2012-06-08 19:23:38 -0600

    Added decorator.

--- termsandconditions/middleware.py
+++ termsandconditions/middleware.py
@@ -1,9 +1,22 @@
 import urlparse
 from django.http import HttpResponseRedirect, QueryDict
 from models import TermsAndConditions
-#from django.conf import settings
+from django.conf import settings
 
-ACCEPT_TERMS_PATH = '/terms/accept/'
+if hasattr(settings, 'ACCEPT_TERMS_PATH'):
+    ACCEPT_TERMS_PATH = settings.ACCEPT_TERMS_PATH
+else:
+    ACCEPT_TERMS_PATH = '/terms/accept/'
+
+if hasattr(settings, 'ACCEPT_TERMS_EXCLUDE_PREFIX_LIST'):
+    ACCEPT_TERMS_EXCLUDE_PREFIX_LIST = settings.ACCEPT_TERMS_EXCLUDE_PREFIX_LIST
+else:
+    ACCEPT_TERMS_EXCLUDE_PREFIX_LIST = {'/admin/',}
+
+if hasattr(settings, 'ACCEPT_TERMS_EXCLUDE_LIST'):
+    ACCEPT_TERMS_EXCLUDE_LIST = settings.ACCEPT_TERMS_EXCLUDE_LIST
+else:
+    ACCEPT_TERMS_EXCLUDE_LIST = {'/',}
 
 class TermsAndConditionslRedirectMiddleware:
     """
@@ -12,8 +25,17 @@ class TermsAndConditionslRedirectMiddleware:
     def process_request(self, request):
         if request.user.is_authenticated():
             if not TermsAndConditions.agreed_to_latest(request.user):
+
                 currentPath = request.META['PATH_INFO']
-                if currentPath != ACCEPT_TERMS_PATH and not currentPath.startswith('/admin'):
+                excludePathFlag = False
+                for excludePath in ACCEPT_TERMS_EXCLUDE_PREFIX_LIST:
+                    if currentPath.startswith(excludePath):
+                        excludePathFlag = True
+
+                if currentPath in ACCEPT_TERMS_EXCLUDE_LIST:
+                    excludePathFlag = True
+
+                if currentPath != ACCEPT_TERMS_PATH and not excludePathFlag:
                     login_url_parts = list(urlparse.urlparse(ACCEPT_TERMS_PATH))
                     querystring = QueryDict(login_url_parts[4], mutable=True)
                     querystring['returnTo'] = currentPath
