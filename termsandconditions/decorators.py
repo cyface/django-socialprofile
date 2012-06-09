eb2f1d1947ddcbc390d3fd3713dc7a7b55fb3e3d
Tim White <tim@cyface.com>
2012-06-08 19:23:38 -0600

    Added decorator.

---
+++ termsandconditions/decorators.py
@@ -0,0 +1,30 @@
+import urlparse
+from django.http import HttpResponseRedirect, QueryDict
+from django.conf import settings
+from django.utils.decorators import available_attrs
+
+if hasattr(settings, 'ACCEPT_TERMS_PATH'):
+    ACCEPT_TERMS_PATH = settings.ACCEPT_TERMS_PATH
+else:
+    ACCEPT_TERMS_PATH = '/terms/accept/'
+
+def terms_required (required_func):
+    """
+    This decorator checks to see if the user is logged in, and if so, if they have accepted the site terms.
+    """
+
+    def decorator(view_func):
+        @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
+            if not request.user.is_authenticated():
+                return view_func(request, *args, **kwargs)
+
+            currentPath = request.META['PATH_INFO']
+            login_url_parts = list(urlparse.urlparse(ACCEPT_TERMS_PATH))
+            querystring = QueryDict(login_url_parts[4], mutable=True)
+            querystring['returnTo'] = currentPath
+            login_url_parts[4] = querystring.urlencode(safe='/')
+            return HttpResponseRedirect(urlparse.urlunparse(login_url_parts))
+
+        return _wrapped_view
+    return decorator
