7cc77baa1f1caf840e1c22c4c79df783b0db6b0c
Tim White <tim@cyface.com>
2012-06-09 21:06:27 -0600

    Moved Secure Testing Views into socialprofile_demo & added 2nd view.

--- socialprofile_demo/views.py
+++ socialprofile_demo/views.py
@@ -1,6 +1,7 @@
 """Django Views for the socialprofile-demo module"""
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 import logging
 
 logger = logging.getLogger(name='socialprofile')
@@ -19,3 +20,35 @@ def index(request):
     response_data = {}
 
     return render_to_response('index.html', response_data, context_instance=RequestContext(request))
+
+@login_required
+def secure_view(request):
+    """
+    Secure testing page.
+
+    url: /secure
+
+    template : templates/secure.html
+    """
+
+    logger.debug('securepage')
+
+    response_data = {}
+
+    return render_to_response('secure.html', response_data, context_instance=RequestContext(request))
+
+@login_required
+def secure_view_too(request):
+    """
+    Secure testing page.
+
+    url: /securetoo
+
+    template : templates/securetoo.html
+    """
+
+    logger.debug('securepagetoo')
+
+    response_data = {}
+
+    return render_to_response('securetoo.html', response_data, context_instance=RequestContext(request))
