030c21bd0cb8ec4c288a692a3e4d690bae97b99e
cyface <tim@cyface.com>
2012-06-12 09:09:36 -0600

    Added logging.captureWarnings(True) settings to send deprecation warnings to the logger.

--- socialprofile_demo/settings_local_template.py
+++ socialprofile_demo/settings_local_template.py
@@ -200,7 +200,6 @@ logging.captureWarnings(True)
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'capturewarnings': True,
     'formatters': {
         'verbose': {
             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
