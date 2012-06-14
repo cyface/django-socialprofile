ab9fd3f041c3917fce5f9c8b6f3a0d915b86fef8
cyface <tim@cyface.com>
2012-06-14 14:27:57 -0600

    Added NAME to database entries.

--- socialprofile_demo/settings_test.py
+++ socialprofile_demo/settings_test.py
@@ -14,6 +14,7 @@ import tempfile
 # Test DB settings. (SQLLite)
 DATABASES = {
     'default': {
+        'NAME': 'default',
         'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': # DO NOT SPECIFY A NAME SO THAT TEST RUNNER WILL USE IN-MEMORY DB
         'SUPPORTS_TRANSACTIONS': 'false',
