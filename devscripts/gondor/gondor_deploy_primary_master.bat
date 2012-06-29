382059cbe78f9c4922ac86f0a92b4925836cec63
cyface <tim@cyface.com>
2012-06-29 15:16:16 -0600

    Gondor config upgrade.

--- devscripts/gondor/gondor_deploy_primary_master.bat
+++ devscripts/gondor/gondor_deploy_primary_master.bat
@@ -1,4 +1,4 @@
-REM This needs to be run from the directory where the .gondor directory lives
+REM This needs to be run from the directory where the gondor.yml file lives
 call django-socialprofile-env\Scripts\activate.bat
 REM This deploys the currently checked-in GIT Master Branch to the primary Gondor Instance
 gondor deploy primary master
