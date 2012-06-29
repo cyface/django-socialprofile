3c995596d7de1268de9d08d770e91b4c48c759af
Brant Steen <brant.steen@gmail.com>
2012-06-29 11:48:00 -0400

    ..

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "django-socialprofile",
