2c6cda170d9af694cf64b881c58826f76d9a2260
cyface <tim@cyface.com>
2012-06-29 15:20:18 -0600

    Gondor config upgrade.

---
+++ socialprofile_demo/settings_gondor.py
@@ -0,0 +1,40 @@
+import os
+import urlparse
+
+from .settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+if "GONDOR_DATABASE_URL" in os.environ:
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["GONDOR_DATABASE_URL"])
+    DATABASES = {
+        "default": {
+            "ENGINE": {
+                "postgres": "django.db.backends.postgresql_psycopg2"
+            }[url.scheme],
+            "NAME": url.path[1:],
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port
+        }
+    }
+
+if "GONDOR_REDIS_URL" in os.environ:
+    urlparse.uses_netloc.append("redis")
+    url = urlparse.urlparse(os.environ["GONDOR_REDIS_URL"])
+    GONDOR_REDIS_HOST = url.hostname
+    GONDOR_REDIS_PORT = url.port
+    GONDOR_REDIS_PASSWORD = url.password
+
+SITE_ID = 1 # set this to match your Sites setup
+
+MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
+STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "static")
+
+MEDIA_URL = "/site_media/media/" # make sure this maps inside of a static_urls URL
+STATIC_URL = "/site_media/static/" # make sure this maps inside of a static_urls URL
+
+FILE_UPLOAD_PERMISSIONS = 0640
