cbcb0d1c40a36d6cd6ea726a7510377230d79ad1
Tim White <tim@cyface.com>
2012-07-01 12:04:26 -0600

    Trying never_cache to see if I can get it to stop caching.

--- socialprofile_demo/views.py
+++ socialprofile_demo/views.py
@@ -1,14 +1,13 @@
 """Django Views for the socialprofile-demo module"""
 from django.shortcuts import render_to_response
-from django.views.decorators.vary import vary_on_cookie
-from django.views.decorators.cache import cache_control
+from django.views.decorators.cache import never_cache
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 import logging
 
 logger = logging.getLogger(name='socialprofile')
 
-@vary_on_cookie
+@never_cache
 def index(request):
     """
     Main site page page.
@@ -25,7 +24,7 @@ def index(request):
     return render_to_response('socialprofile_demo/index.html', response_data, context_instance=RequestContext(request))
 
 @login_required
-@cache_control(must_revalidate=True, max_age=3600)
+@never_cache
 def secure_view(request):
     """
     Secure testing page.
@@ -42,7 +41,7 @@ def secure_view(request):
     return render_to_response('socialprofile_demo/secure.html', response_data, context_instance=RequestContext(request))
 
 @login_required
-@cache_control(must_revalidate=True, max_age=3600)
+@never_cache
 def secure_view_too(request):
     """
     Secure testing page.
