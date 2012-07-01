daff694785494b0d959e2bd7101c51fb8ea8127e
Tim White <tim@cyface.com>
2012-07-01 11:21:26 -0600

    Adding cache control decorators to see if that prevents unfortunate caching on gondor...

--- socialprofile_demo/views.py
+++ socialprofile_demo/views.py
@@ -1,11 +1,14 @@
 """Django Views for the socialprofile-demo module"""
 from django.shortcuts import render_to_response
+from django.views.decorators.vary import vary_on_cookie
+from django.views.decorators.cache import cache_control
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 import logging
 
 logger = logging.getLogger(name='socialprofile')
 
+@vary_on_cookie
 def index(request):
     """
     Main site page page.
@@ -22,6 +25,7 @@ def index(request):
     return render_to_response('socialprofile_demo/index.html', response_data, context_instance=RequestContext(request))
 
 @login_required
+@cache_control(must_revalidate=True, max_age=3600)
 def secure_view(request):
     """
     Secure testing page.
@@ -38,6 +42,7 @@ def secure_view(request):
     return render_to_response('socialprofile_demo/secure.html', response_data, context_instance=RequestContext(request))
 
 @login_required
+@cache_control(must_revalidate=True, max_age=3600)
 def secure_view_too(request):
     """
     Secure testing page.
