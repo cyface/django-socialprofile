a798c1576e36591aeccd1d2bd7ce439695167f7e
cyface <tim@cyface.com>
2012-07-05 11:02:25 -0600

    Removed Terms & Conditions Module and API Keys, preparing for public exposure.

--- devscripts/gondor/gondor_primary_clear_database.bat
+++
@@ -1,4 +0,0 @@
-REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
-call django-socialprofile-env\Scripts\activate.bat
-REM CLEARS DATABASE - AS IN DELETE ALL from primary Gondor Instance
-gondor manage primary database:clear
