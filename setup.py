5bb8cc9b46575f3f9a7eceefb4a4125f76b97743
cyface <tim@cyface.com>
2012-07-05 13:27:33 -0600

    Removed gondor settings file. Adjusted language in setup.py.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version = ".1",
     url = 'http://timlwhite.com',
     license = 'GPL',
-    description = "django-socialprofile enables users to manage their user profile built by logging in via a social service such as Google, Twitter, or Facebook.",
+    description = "django-socialprofile enables users to manage their user profile built by logging in via a social service such as Google, Twitter, or Facebook. It relies on django-socialauth.",
     long_description = open('README.rst').read(),
 
     author = 'Tim White',
     author_email = 'tim@cyface.com',
     
-    packages = find_packages(exclude=('socialprofile_demo', 'tests', )),
+    packages = find_packages(exclude=('socialprofile_demo', 'tests', 'devscripts' )),
     include_package_data = True,
     zip_safe = False,
 
