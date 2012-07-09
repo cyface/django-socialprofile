a3705f44dd772d4c97c360a19617f5dc680438c5
cyface <tim@cyface.com>
2012-07-09 08:03:03 -0600

    Added PIP_DOWNLOAD_CACHE to windows virtualenv script.

--- devscripts/virtualenv/setup_dev_env.bat
+++ devscripts/virtualenv/setup_dev_env.bat
@@ -3,6 +3,8 @@ REM Assuming you have python installed, you can install virtualenv using the com
 REM curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 REM This should be run from the project directory, not inside the socialprofile dir
 
+SET PIP_DOWNLOAD_CACHE=..\pip_download_cache
+
 virtualenv --system-site-packages django-socialprofile-env
 call django-socialprofile-env\Scripts\activate.bat
 pip install -r socialprofile_demo/requirements_dev.txt
