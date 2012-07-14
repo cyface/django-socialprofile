10c3bf646eb2dce7d32ba75525b5401187220bff
Tim White <tim@cyface.com>
2012-07-13 22:14:49 -0600

    Pylint cleanup.

--- devscripts/checkers/run_checks.sh
+++ devscripts/checkers/run_checks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ./django-socialprofile-env/bin/activate
-python manage.py jenkins --pylint-rcfile=devscripts/checkers/pylintrc --coverage-rcfile=devscripts/checkers/coveragerc --csslint-interpreter=rhino socialprofile
+python manage.py jenkins --pylint-rcfile=devscripts/checkers/pylintrc --coverage-rcfile=devscripts/checkers/coveragerc --csslint-interpreter=node --csslint-implementation=/usr/bin/csslint --jslint-interpreter=node socialprofile
