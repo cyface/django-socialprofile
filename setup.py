34b04179ab58cf037fd7edc9acb0533669e04cae
Tim White <tim@cyface.com>
2012-07-24 19:24:07 -0600

    Fixed CSRF Token on Delete Form.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-socialprofile",
-    version=".1.6",
+    version="0.1.6",
     url='http://timlwhite.com',
     license='BSD',
     description="django-socialprofile enables users to manage their user profile built by logging in via a social service such as Google, Twitter, or Facebook. It relies on django-socialauth."
