6c2c94a31600a695b962bb5a2c409b43ac9a1da0
Tim White <tim@cyface.com>
2014-04-19 12:39:44 -0600

    fixing older django_jenkins config.

--- socialprofile_demo/settings.py
+++ socialprofile_demo/settings.py
@@ -297,8 +297,7 @@ LOGGING = {
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.django_tests',
-    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_jshint',
     'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount',
 )
